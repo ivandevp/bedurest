171ab22ba3eb84928443d62afda2c9fa70344e26
ivandevp <medinadiazivan@gmail.com>
2020-07-04 11:41:42 -0500

    Add lifecycle methods to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 // Componente
 function App() {
-  const { pathname } = window.location;
+  const [checked, setChecked] = React.useState(true);
+  const { pathname, search } = window.location;
 
   if (pathname === "/login") {
     return <Login />;
   }
 
-  return <Home />;
+  if (pathname === "/dashboard") {
+    return <Dashboard />;
+  }
+
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          onChange={(event) => {
+            setChecked(!event.target.checked);
+          }}
+        />
+        Desmontar componente
+      </label>
+      {checked ? <Home name={search.slice(6)} /> : null}
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+// props: { name: "Ivan" }
 export default class Home extends React.Component {
+  // Inicializar estados, bindear eventos, declarar atributos globales
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    console.log("==CONSTRUCTOR==");
+  }
+
+  // Requests iniciales, iniciar sockets, hacer cálculos para modificar el DOM
+  componentDidMount() {
+    this.setState({
+      counter: 1,
+    });
+
+    console.log("==COMPONENT DID MOUNT==");
+  }
+
+  // Para optimizar el performance
+  shouldComponentUpdate() {
+    console.log("==SHOULD COMPONENT UPDATE?==");
+    return true;
+  }
+
+  // Actualizar estados basados en condiciones
+  componentDidUpdate() {
+    console.log("==COMPONENT DID UPDATE==");
+  }
+
+  // Eliminar conexiones abiertas y evitar memory leaks
+  componentWillUnmount() {
+    console.log("==COMPONENT WILL UNMOUNT==");
+  }
+
+  // Lleva toda la interfaz que se ejecuta una o múltiples veces
   render() {
-    return <h1>Home</h1>;
+    // const name = this.props.name;
+    console.log("==RENDER==");
+
+    return <h1>Home {this.props.name}</h1>;
   }
 }
