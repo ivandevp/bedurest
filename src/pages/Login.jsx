a6261d55cc121d7e09263df6e81cdb09f3eb5802
ivandevp <medinadiazivan@gmail.com>
2020-07-04 12:16:05 -0500

    Add initial state for email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,40 @@ import React, { Component } from "react";
 import "./Login.css";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  login = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  updateEmail = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <h2 className="login-title">LOGIN</h2>
         <form className="login-form">
           <div className="form-controls">
-            <input type="email" placeholder="Correo electrónico" />
-            <input type="password" placeholder="Contraseña" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Correo electrónico"
+              value={this.state.email}
+              onChange={this.updateEmail}
+            />
+            <input name="password" type="password" placeholder="Contraseña" />
           </div>
           <div className="form-actions">
             <div>
@@ -19,7 +45,9 @@ export default class Login extends Component {
               </label>
               <a href="#">Forgot?</a>
             </div>
-            <button type="submit">Iniciar sesión</button>
+            <button type="submit" onClick={this.login}>
+              Iniciar sesión
+            </button>
           </div>
         </form>
       </div>
