75c835542a0810c74c60cbaa70d789ac27f320e7
ivandevp <medinadiazivan@gmail.com>
2020-07-11 10:20:57 -0500

    Remove will unmount demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,24 @@ import React from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
 import "./App.css";
 
 // Componente
 function App() {
-  const [checked, setChecked] = React.useState(true);
   const { pathname, search } = window.location;
 
-  if (pathname === "/login") {
-    return <Login />;
+  switch (pathname) {
+    case "/login":
+      return <Login />;
+    case "/dashboard":
+      return <Dashboard />;
+    case "/signup":
+      return <Signup />;
+    case "/home":
+    default:
+      return <Home name={search.slice(6)} />;
   }
-
-  if (pathname === "/dashboard") {
-    return <Dashboard />;
-  }
-
-  return (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          onChange={(event) => {
-            setChecked(!event.target.checked);
-          }}
-        />
-        Desmontar componente
-      </label>
-      {checked ? <Home name={search.slice(6)} /> : null}
-    </>
-  );
 }
 
 export default App;
